4ef35bd433d851c28d02fdaaaab2d5f070c0c083
matteo-stat <fogliata.teo@gmail.com>
2024-08-18 21:44:41 +0200

    added encode image function for test set

--- mobilevitlib/processing.py
+++ mobilevitlib/processing.py
@@ -30,6 +30,28 @@ def encode_image_and_label(image_file_path: str, label: int, target_resolution: 
 
     return image, label
 
+def encode_image(image_file_path: str, target_resolution: Tuple[int, int] = (256, 256)) -> tf.Tensor:
+    """
+    load jpg image resizing or cropping it to a target resolution and one hot encode a label
+
+    Args:
+        image_file_path (str): file path name of the jpeg image to load
+        target_resolution (Tuple[int, int]): a tuple containing the target resolution for images (width, height)
+
+    Returns:
+        Tuple[tf.Tensor, tf.Tensor]: image data, label one hot encoded
+    """
+    # load image
+    image = tf.io.read_file(image_file_path)
+    image = tf.image.decode_jpeg(image)
+    image = tf.cast(image, dtype=tf.float32)
+
+    # rescale or randomly crop to target size
+    rescaling_factor = random.uniform(1, 1.25)
+    image = tf.image.resize(image, size=(int(target_resolution[0]*rescaling_factor), int(target_resolution[1]*rescaling_factor)))
+    image = tf.image.random_crop(image, size=(target_resolution[0], target_resolution[1], 3))                            
+
+    return image
 
 def data_augmentation(images_batch: tf.Tensor, labels_batch: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor]:
     """
